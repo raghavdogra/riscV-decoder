88d37867cf83dbb9fb40c81c13a321d461f3ffa3
Raghav Dogra <raghavdogra@gmail.com>
2017-02-03 03:05:48 -0500

    Decoder: add 64IM support
    Signed-off-by: Raghav Dogra <raghavdogra@gmail.com>

--- decoder.sv
+++ decoder.sv
@@ -5,7 +5,6 @@ getreg gr_name();
 task decode;
 input [31:0] lower;
 input [63:0] pc;
-logic [2:0] data_index;
 logic [32:0] rd,rs1,rs2;
 logic [64:0] opcode;
 begin
@@ -13,18 +12,27 @@ begin
                 gr_name.convert(lower[11:7],rd);
                 gr_name.convert(lower[19:15],rs1);
                 gr_name.convert(lower[24:20],rs2);
-                case({lower[30], lower[14:12]})
-                        4'b0000: opcode = "add";
-                        4'b1000: opcode = "sub";
-                        4'b0001: opcode = "sll";
-                        4'b0010: opcode = "slt";
-                        4'b0110: opcode = "sltu";
-                        4'b0100: opcode = "xor";
-                        4'b1101: opcode = "sra";
-                        4'b0000: opcode = "or";
-                        4'b0000: opcode = "and";
+                case({lower[30], lower[25], lower[14:12]})
+                        5'b00000: opcode = "add";
+                        5'b10000: opcode = "sub";
+                        5'b00001: opcode = "sll";
+                        5'b00010: opcode = "slt";
+                        5'b00110: opcode = "sltu";
+                        5'b00100: opcode = "xor";
+                        5'b10101: opcode = "sra";
+                        5'b00101: opcode = "srl";
+                        5'b00110: opcode = "or";
+                        5'b00111: opcode = "and";
+                        5'b01000: opcode = "mul";
+                        5'b01001: opcode = "mulh";
+                        5'b01010: opcode = "mulhsu";
+                        5'b01011: opcode = "mulhu";
+                        5'b01100: opcode = "div";
+                        5'b01101: opcode = "divu";
+                        5'b01110: opcode = "rem";
+                        5'b01111: opcode = "remu";
                 endcase
-                $display ("%x:  %x              %s      %s,%s,%s", pc, lower,opcode,rd,rs1,rs2);
+                $display ("%x:  %x          	%s      %s,%s,%s", pc, lower,opcode,rd,rs1,rs2);
         end else if (lower[6:0] == 7'b0010011) begin
                 gr_name.convert(lower[11:7],rd);
                 gr_name.convert(lower[19:15],rs1);
@@ -36,12 +44,12 @@ begin
                         3'b110: opcode = "ori";
                         3'b111: opcode = "andi";
                 endcase
-                $display ("%x:  %x              %s      %s,%s,%d", pc , lower,opcode,rd,rs1,lower[31:20]);
                 case({lower[30], lower[14:12]})
                         4'b0001: opcode = "slli";
                         4'b0101: opcode = "srli";
                         4'b1101: opcode = "srai";
                 endcase
+                $display ("%x:  %x              %s      %s,%s,%d", pc , lower,opcode,rd,rs1,lower[31:20]);
         end else if (lower[6:0] == 7'b1110011) begin
                 gr_name.convert(lower[11:7],rd);
                 gr_name.convert(lower[19:15],rs1);
@@ -62,8 +70,10 @@ begin
                         3'b010: opcode = "lw";
                         3'b100: opcode = "lbu";
                         3'b101: opcode = "lhu";
+			3'b011: opcode = "ld";
+			3'b110: opcode = "lwu";
                 endcase
-                $display("%x:	%x		%s            %s,%s,%d",pc,lower,opcode,rd,rs1,lower[31:20]);
+                $display ("%x:  %x              %s      %s,%s,%d", pc , lower,opcode,rd,rs1,lower[31:20]);
         end else if (lower[6:0] == 7'b1100011) begin
                 gr_name.convert(lower[24:20],rs2);
                 gr_name.convert(lower[19:15],rs1);
@@ -76,6 +86,46 @@ begin
                         3'b110: opcode = "bltu";
                         3'b111: opcode = "bgeu";
                endcase
+        end else if (lower[6:0]  == 7'b0100011) begin
+                gr_name.convert(lower[19:15],rs1);
+                gr_name.convert(lower[24:20],rs2);
+                case (lower[14:12])
+                        3'b000: opcode = "sb";
+                        3'b001: opcode = "sh";
+                        3'b010: opcode = "sw";
+                        3'b011: opcode = "sd";
+                endcase
+                $display ("%x:  %x              %s      %s,%s", pc, lower,opcode,rs1,rs2);
+
+         end else if (lower[6:0] == 7'b0111011) begin
+                gr_name.convert(lower[11:7],rd);
+                gr_name.convert(lower[19:15],rs1);
+                gr_name.convert(lower[24:20],rs2);
+                case({lower[30],lower[25], lower[14:12]})
+                        5'b00000: opcode = "addw";
+                        5'b10000: opcode = "subw";
+                        5'b00001: opcode = "sllw";
+                        5'b00101: opcode = "srlw";
+                        5'b10101: opcode = "sraw";
+                        5'b01000: opcode = "mulw";
+                        5'b01100: opcode = "divw";
+                        5'b01101: opcode = "divwu";
+                        5'b01110: opcode = "remw";
+                        5'b01111: opcode = "remuw";
+                endcase
+                $display ("%x:  %x              %s      %s,%s,%s", pc, lower,opcode,rd,rs1,rs2);
+        end else if (lower[6:0] == 7'b0011011) begin
+                gr_name.convert(lower[11:7],rd);
+                gr_name.convert(lower[19:15],rs1);
+                case(lower[14:12])
+                        3'b000: opcode = "addiw";
+                endcase
+                case({lower[30], lower[14:12]})
+                        4'b0001: opcode = "slliw";
+                        4'b0101: opcode = "srliw";
+                        4'b1101: opcode = "sraiw";
+                endcase
+                $display ("%x:  %x              %s      %s,%s,%d", pc , lower,opcode,rd,rs1,lower[31:20]);
         end else begin
               $display("unknown at this time  ");
         end
